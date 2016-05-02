65d8991e17dda1f18e7aaecffb3a0c5de37a09f7
blues <blues@blues.com>
2016-05-02 11:25:10 +0800

    udacity alien finish

---
+++ beginning-ios-robot-maze-2-robot-maze-2-part-2/RobotMaze2/Maze/WallLocations.swift
@@ -0,0 +1,82 @@
+//
+//  CollectingDataAboutWalls.swift
+//  Maze
+//
+//  Created by Gabrielle Miller-Messner on 11/6/15.
+//  Copyright © 2015 Udacity, Inc. All rights reserved.
+//
+
+import Foundation
+
+extension ControlCenter {
+    
+    func isFacingWall(robot: ComplexRobotObject, direction: MazeDirection) -> Bool {
+        
+        let cell = mazeController.currentCell(robot)
+        var isWall: Bool = false
+        
+        // You may want to paste your Part 1 implementation of isFacingWall() here
+        switch(direction) {
+        case .Up:
+            if cell.top {
+                isWall = true
+            }
+        case .Right:
+            if cell.right {
+                isWall = true
+            }
+        case .Down:
+            if cell.bottom {
+                isWall = true
+            }
+        case .Left:
+            if cell.left {
+                isWall = true
+            }
+        }
+        
+        // Step 1.1b
+        // TODO: Return a Bool that represents whether the robot is currently facing a wall. You will need to change the placeholder return statement below.
+        return isWall
+        
+    }
+    
+    func checkWalls(robot:ComplexRobotObject) -> (up: Bool, right: Bool, down: Bool, left: Bool, numberOfWalls: Int) {
+        var numberOfWalls = 0
+        let cell = mazeController.currentCell(robot)
+        
+        // Check is there is a wall at the top of the current cell
+        let isWallUp = cell.top
+        if isWallUp {
+            numberOfWalls++
+        }
+        
+        // Check if there is a wall to the right of the current cell
+        let isWallRight = cell.right
+        if isWallRight {
+            numberOfWalls++
+        }
+        
+        // Step 2.1a
+        // TODO: Check if there is a wall at the bottom of the current cell
+        let isWallBottom = cell.bottom
+        if isWallBottom {
+            numberOfWalls++
+        }
+        
+        // TODO: Check if there is a wall to the left of the current cell
+        let isWallLeft = cell.left
+        if isWallLeft {
+            numberOfWalls++
+        }
+        
+        
+        // Step 2.1b
+        // TODO: Test the checkWalls function.
+        print(isWallUp , isWallRight , isWallBottom , isWallLeft , numberOfWalls)
+        
+        // TODO: Return a tuple representing the bools for top, right, down & left, and the number of walls
+        // This tuple is a placeholder
+        return (isWallUp , isWallRight , isWallBottom , isWallLeft , numberOfWalls)
+    }
+}
